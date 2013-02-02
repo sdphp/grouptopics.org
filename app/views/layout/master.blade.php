2831870794aec3a5a9549a4b08def4a2c26fb39f
root <root@ip-10-197-0-172.us-west-1.compute.internal>
2013-02-02 20:33:52 +0000

    Adding twitter bootstrap

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,10 +1,16 @@
 <html>
 	<head>
-	
+		<link type="text/css" href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<link type="text/css" href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+		
 	</head>
 	<body>
+		@include('layout.menu')
 		<div class="container">
 			@yield('content')
 		</div>
+
+		<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
