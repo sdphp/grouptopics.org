29b59a8bd4c1f014c32d9601c73695992b53828d
John Congdon <john@johncongdon.com>
2013-02-02 06:22:14 -0800

    Added a powered by image

--- public/index.php
+++ public/index.php
@@ -2,4 +2,13 @@
 
 // A place holder to get the app launched on orchestra.io
 
-echo "Index";
+echo <<<EOF
+<html>
+<head>
+<title>Group Topics</title>
+</head>
+<body>
+Powered by<br/>
+<img src="/images/sd-php-logo.png"/>
+</body>
+</html>
