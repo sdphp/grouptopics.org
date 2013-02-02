9c1be06d1355aaa17e3ded26048c4a9e9f3eaf5c
root <root@ip-10-197-0-172.us-west-1.compute.internal>
2013-02-02 20:00:16 +0000

    Adding blade templating

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,9 @@
 <?php
 
-class BaseController extends Controller {
+abstract class BaseController extends Controller {
+
+
+	protected $layout = 'layout.master';
 
 	/**
 	 * Setup the layout used by the controller.
@@ -13,6 +16,28 @@ class BaseController extends Controller {
 		{
 			$this->layout = View::make($this->layout);
 		}
+	}
+
+	protected function render( $view = '' )
+	{
+		if( empty( $view ) ) {
+			$controller = strtolower( str_replace( 'Controller', '', get_class( $this ) ) );
+	
+			$action = $this->parseAction();
+			$view = $controller . '.' . $action;
+		}
+
+		$this->layout->content = View::make( $view );
+
+		return true;
+	}
+
+	private function parseAction()
+	{
+		$trace = debug_backtrace();
+		$action = $trace[ 2 ];
+	
+		return $action[ 'function' ];
 	}
 
 }
