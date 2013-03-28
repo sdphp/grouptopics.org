d95f3313e75a528d07b14170ec11b6b351a7bb7f
William Cahill-Manley <william@cahillmanley.com>
2013-03-27 22:21:13 -0700

    Remove Unneeded Route Groups

--- site/app/config/routes/security.php
+++ site/app/config/routes/security.php
@@ -10,33 +10,10 @@
 */
 
 
-// Security Routes
-Route::group(
-    array( 'as' => 'securityRoutes' ),
-    function () {
 
-        // Login
-        Route::group(
-            array( 'as' => 'loginGroup' ),
-            function () {
-                Route::get( 'login', array( 'as' => 'login', 'uses' => 'SecurityController@login' ) );
-                Route::post( 'login', array( 'as' => 'login_post', 'uses' => 'SecurityController@loginSubmit' ) );
-            }
-        );
+Route::get( 'login', array( 'as' => 'login', 'uses' => 'SecurityController@login' ) );
+Route::post( 'login', array( 'as' => 'login_post', 'uses' => 'SecurityController@loginSubmit' ) );
 
-        Route::group(
-            array( 'as' => 'providerGroup' ),
-            function () {
-                Route::get( 'login/provider/{provider}', array( 'as'   => 'providerLogin', 'uses' => 'SecurityController@provider' ) );
-            }
-        );
+Route::get( 'login/provider/{provider}', array( 'as'   => 'providerLogin', 'uses' => 'SecurityController@provider' ) );
 
-        // Logout
-        Route::group(
-            array( 'as' => 'logoutGroup' ),
-            function () {
-                Route::get( 'logout', array( 'as' => 'logout', 'uses' => 'SecurityController@logout' ) );
-            }
-        );
-    }
-);
+Route::get( 'logout', array( 'as' => 'logout', 'uses' => 'SecurityController@logout' ) );
