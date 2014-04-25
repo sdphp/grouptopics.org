2f1cf3e8bae70432b6cd85edd6ee699f79040a60
Jason McCallister <themccallister@gmail.com>
2014-04-25 16:12:37 -0700

    Modify Gulp
    Modify gulpfile.js to point to new directory “/less”.
    Create “/less/grouptopics.less” which imports bootstrap.less.
    Update readme to detect language in code block.

--- app/views/_layouts/default.blade.php
+++ app/views/_layouts/default.blade.php
@@ -6,8 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Group Topics</title>
-  <link rel="stylesheet" href="/css/bootstrap.css">
-  <link rel="stylesheet" href="/css/custom.css">
+  <link rel="stylesheet" href="/css/grouptopics.css">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
