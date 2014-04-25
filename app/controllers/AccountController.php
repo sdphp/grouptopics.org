8167526c2406a1f8d3c571383b8158cc61710379
Jason McCallister <themccallister@gmail.com>
2014-04-25 01:16:07 -0700

    Update namespace, add docblocks, add accountSettings and showAccountById

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -1,48 +1,88 @@
 <?php
 
-use Grouptopics\Repositories\AccountRepository;
+use Grouptopics\Libraries\Account;
 
+/**
+ * Class AccountController
+ */
 class AccountController extends BaseController
 {
-	protected $account;
+    /**
+     * @var Grouptopics\Libraries\Account
+     */
+    protected $account;
 
-	public function __construct(AccountRepository $account)
+    /**
+     * @param AccountLibrary $account
+     */
+    public function __construct(Account $account)
 	{
 		$this->account = $account;
 	}
 
-	public function index()
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function account()
 	{
 		return View::make('account');
 	}
 
-	public function signup()
+    public function accountSettings()
+    {
+        return View::make('account_settings');
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function signup()
 	{
 		return View::make('signup');
 	}
 
-	public function login()
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function login()
 	{
 		return View::make('login');
 	}
 
-	public function create()
+    /**
+     * @return string
+     */
+    public function create()
 	{
 		return $this->account->createNewAccount();
 	}
 
-	public function store()
+    /**
+     * @return mixed
+     */
+    public function store()
 	{
 		return $this->account->loginExistingAccount();
 	}
-	public function destroy()
+
+    /**
+     * @return mixed
+     */
+    public function destroy()
 	{
 		return $this->account->logoutExistingAccount();
 	}
 
-	public function testMe()
-	{
-		return "Grouptopics\Repositories\AccountRepository";
-	}
+    public function showAccountById($id)
+    {
+        $user = User::find($id);
+        return $user->name;
+    }
+
+    public function showAccountByUsername($username)
+    {
+        $user = User::find($username);
+        return $user->name;
+    }
 
 }
