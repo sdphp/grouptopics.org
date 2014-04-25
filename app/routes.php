032bc74bd82db5de5970fb16578045056c6e9e8f
Jason McCallister <themccallister@gmail.com>
2014-04-25 13:08:59 -0700

    PSR it up
    Convert to spaces as well.

--- app/routes.php
+++ app/routes.php
@@ -1,71 +1,102 @@
 <?php
 
 // GET requests
-Route::get('/', array(
-		'as' => 'home',
-		'uses' => 'ViewController@pageHome'
-));
+Route::get(
+    '/',
+    array(
+        'as' => 'home',
+        'uses' => 'ViewController@pageHome'
+    )
+);
 
-Route::get('/login', array(
-	'as' => 'login',
-	'uses' => 'ViewController@pageLogin'
-));
+Route::get(
+    '/login',
+    array(
+        'as' => 'login',
+        'uses' => 'ViewController@pageLogin'
+    )
+);
 
-Route::get('/about', array(
-	'as' => 'about',
-	'uses' => 'ViewController@pageAbout'
-));
+Route::get(
+    '/about', array(
+        'as' => 'about',
+        'uses' => 'ViewController@pageAbout'
+    )
+);
 
-Route::get('/signup', array(
-	'as'=> 'signup',
-	'uses' => 'ViewController@pageSignup'
-));
+Route::get(
+    '/signup', array(
+        'as'=> 'signup',
+        'uses' => 'ViewController@pageSignup'
+    )
+);
 
-Route::get('/logout', array(
-	'as' => 'action.logout',
-	'uses' => 'AccountController@actionLogout'
-));
+Route::get(
+    '/logout', array(
+        'as' => 'action.logout',
+        'uses' => 'AccountController@actionLogout'
+    )
+);
 
-Route::get('/profile/{username}', array(
-	'as' => 'profile.view',
-	'uses' =>'AccountController@profileView'
-));
+Route::get(
+    '/profile/{username}',
+        array(
+            'as' => 'profile.view',
+            'uses' =>'AccountController@profileView'
+        )
+);
 
 // GROUP requests requiring Auth
-Route::group(array(
-	'before' => 'auth'
-), function()
-{
-	Route::get('/account', array(
-		'as' => 'auth.account',
-		'uses' => 'AccountController@account'
-	));
+Route::group(
+    array(
+        'before' => 'auth'
+    ),
+    function() {
+        Route::get(
+            '/account', array(
+                'as' => 'auth.account',
+                'uses' => 'AccountController@account'
+            )
+        );
 
-    Route::get('/account/settings', array(
-		'as' => 'auth.account.settings',
-		'uses' => 'AccountController@accountSettings'
-	));
-});
+    Route::get(
+        '/account/settings', array(
+            'as' => 'auth.account.settings',
+            'uses' => 'AccountController@accountSettings'
+            )
+        );
+    }
+);
 
 
 // POST requests
-Route::post('/signup', array(
-	'as' => 'action.signup',
-	'uses' => 'AccountController@create'
-));
+Route::post(
+    '/signup',
+        array(
+            'as' => 'action.signup',
+            'uses' => 'AccountController@create'
+        )
+);
 
-Route::post('/login', array(
-	'as' => 'action.login',
-	'uses' => 'AccountController@store'
-));
+Route::post(
+    '/login',
+        array(
+            'as' => 'action.login',
+            'uses' => 'AccountController@store'
+        )
+);
 
 // GROUP request that require admin privileges
-Route::group(array(
-	'prefix' => 'manage'
-), function()
-{
-    Route::get('manage', function()
-    {
-    	return "ADMIN!";
-    });
-});
+Route::group(
+    array(
+        'prefix' => 'manage'
+    ),
+    function() {
+        Route::get(
+            'manage',
+            function() {
+                return "ADMIN!";
+            }
+        );
+    }
+);
