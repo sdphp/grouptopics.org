08a7aed45de17a24277c236cd8dbda52d78fa710
Jason McCallister <themccallister@gmail.com>
2014-04-25 01:13:13 -0700

    Add account/settings and profile to routes.php

--- app/routes.php
+++ app/routes.php
@@ -2,24 +2,43 @@
 
 // GET requests
 Route::get('/', 'ViewController@index');
-Route::get('/about', 'ViewController@about');
-Route::get('/login', 'AccountController@login');
-Route::get('/signup', 'AccountController@signup');
-Route::get('/account', 'AccountController@index')->before('auth');
+Route::get('/about', array('as' => 'about', 'uses' => 'ViewController@about'));
+Route::get('/login', array('as' => 'login', 'uses' => 'AccountController@login'));
+Route::get('/signup', array('as' => 'signup', 'uses' => 'AccountController@signup'));
 Route::get('/logout', 'AccountController@destroy');
+// show the public profile, votes, topics, presentations and etc.
+Route::get('/profile/{username}', 'AccountController@showPublicProfile');
 
-// JUNK - for testing specific functions as needed
-Route::get('utils', function()
+
+// GROUPS requests requiring Auth
+Route::group(array('before' => 'auth'), function()
 {
-	// dd mysql configuration from database.php
-	dd(Config::get('database.connections.mysql'));
-	// dd(App::environment());
-
-	// $users = User::all();
-	// dd($users);
-
+	Route::get('/account', 'AccountController@account');
+    Route::get('/account/settings', 'AccountController@accountSettings');
 });
 
 // POST requests
 Route::post('/signup', 'AccountController@create');
 Route::post('/login', 'AccountController@store');
+
+
+// GROUPS request that require admin privileges
+Route::group(array('prefix' => 'manage'), function()
+{
+    Route::get('manage', function()
+    {
+    	return "ADMIN!";
+    });
+});
+
+
+// JUNK - for testing specific functions as needed
+Route::get('utils', function()
+{
+    // dd mysql configuration from database.php
+    // dd(Config::get('database.connections.mysql'));
+    // dd(App::environment());
+
+    // $users = User::all();
+    // dd($users);
+});
