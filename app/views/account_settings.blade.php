1cd25cc18d837b4090539d9e63f4c6dcbd07c8b9
Jason McCallister <themccallister@gmail.com>
2014-04-25 20:37:03 -0700

    Remove functionality from account_settings

--- app/views/account_settings.blade.php
+++ app/views/account_settings.blade.php
@@ -4,25 +4,6 @@
 
 	<h1>Settings for {{Auth::user()->name}}!</h1>
 
-	{{ Form::open(array('url' => 'account/settings')) }}
-
-	<div class="form-group">
-		{{ Form::label('email', 'E-Mail Address', array('class' => 'awesome')) }}
-		{{ Form::text('email', null, array('class' => 'form-control')) }}
-	</div>
-
-	<div class="form-group">
-
-		{{ Form::label('password', 'Password', array('class' => 'awesome')) }}
-		{{ Form::text('email', null, array('class' => 'form-control')) }}
-
-		{{ Form::label('password_confirmation', 'Confirm', array('class' => 'awesome')) }}
-		{{ Form::text('password_confirmation', null, array('class' => 'form-control')) }}
-
-	</div>
-
-	{{ Form::close() }}
-
 @stop
 
 @section('sidebar')
