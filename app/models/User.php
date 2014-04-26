b34ce98ee57606a173b7423773e3e27a0dcee73e
Jason McCallister <themccallister@gmail.com>
2014-04-25 20:29:45 -0700

    Add permissions to $hidden

--- app/models/User.php
+++ app/models/User.php
@@ -17,7 +17,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('password');
+	protected $hidden = array('password', 'permissions');
 
 	/**
 	 * Get the unique identifier for the user.
