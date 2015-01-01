f3c194f9d3e6da7e470a82d98e11036c7dadaf6b
Eric Van Johnson <eric@esolves.com>
2015-01-01 12:26:49 -0800

    Changing Environment Detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(
     function () {
-        return getenv('ENV') ?: 'development';
+        return getenv('APP_ENV') ?: 'localhost';
     }
 );
 
